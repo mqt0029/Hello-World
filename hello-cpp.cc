ba7b58cd4f9c34b8300a5c4211bedfc2f8bfeeb2
Minh Tram <jmt.unitedstates@gmail.com>
2020-08-18 15:15:49 -0500

    added library version string output

--- hello-cpp.cc
+++ hello-cpp.cc
@@ -1,12 +1,28 @@
 #include <iostream>
-#include <boost/array.hpp>
-// #include <gtkmm.h>
+#include <boost/version.hpp>
+#include <opencv4/opencv2/core/version.hpp>
+#include <dlib/revision.h>
+#include <librealsense2/rs.h>
 
 int main(int argc, char const *argv[])
 {
-    // std::cout << "Hello World!" << std::endl;
+    std::cout << "Using OpenCV " << CV_VERSION << std::endl;
 
-    // std::cout << "Hello World!\n";   
+    std::cout << "Using Intel RealSense Library "     
+              << RS2_API_FULL_VERSION_STR
+              << std::endl;
+
+    std::cout << "Using dlib "     
+              << DLIB_MAJOR_VERSION << "."  // major version
+              << DLIB_MINOR_VERSION << "."  // minor version
+              << DLIB_PATCH_VERSION         // patch level
+              << std::endl;
+
+    std::cout << "Using Boost "     
+              << BOOST_VERSION / 100000     << "."  // major version
+              << BOOST_VERSION / 100 % 1000 << "."  // minor version
+              << BOOST_VERSION % 100                // patch level
+              << std::endl;
 
     printf( "Hello World!\n" ); 
 
